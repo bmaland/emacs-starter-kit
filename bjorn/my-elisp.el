6863eb414996384880f4aa5d67a57be867b2fa07
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2011-02-17 12:01:10 +0100

    Couple of rails elisp functions

--- bjorn/my-elisp.el
+++ bjorn/my-elisp.el
@@ -1,5 +1,19 @@
 ;;; my-elisp.el --- Various pieces of elisp created by myself and others
 
+(defun is-rails-project ()
+  (when (textmate-project-root)
+    (file-exists-p (expand-file-name "config/environment.rb" (textmate-project-root)))))
+
+(defun run-rails-test-or-ruby-buffer ()
+  (interactive)
+  (if (is-rails-project)
+      (let* ((path (buffer-file-name))
+             (filename (file-name-nondirectory path))
+             (test-path (expand-file-name "test" (textmate-project-root)))
+             (command (list ruby-compilation-executable "-I" test-path path)))
+        (pop-to-buffer (ruby-compilation-do filename command)))
+    (ruby-compilation-this-buffer)))
+
 (defun inconsolata (size)
   (interactive "p")
   (set-default-font
