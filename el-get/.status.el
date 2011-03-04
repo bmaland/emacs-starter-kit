e3edb2c292e0fdbf1e5eb759422907d4bfdd075e
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2011-03-04 15:00:21 +0100

    Remove el-get installed packages from git

--- el-get/.status.el
+++
@@ -1,1 +0,0 @@
-(:rvm "installed" :rhtml "installed" :textmate "installed" :el-get "installed" :ipython "installed" :python-mode "installed" :pylookup "installed" :pymacs "installed" :ropemacs "installed" :color-theme-tango-2 "installed" :auto-complete "installed" :anything "installed" :autopair "installed" :color-theme-desert "installed" :magit "required" :quack "installed" :ack "removed")
